6d36c1556b95930459d938f2b87600d45ca63187
Halil Koca <ihalilkoca@gmail.com>
2020-01-23 17:05:12 +0300

    0, 2 bug fix

---
+++ src/0_Util/GenericExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _0_Util
+{
+    public static class GenericExtensions
+    {
+        public static void WriteToConsole(this string message, object arg0 = null)
+        {
+            string formatted = "";
+            if (!string.IsNullOrWhiteSpace(message))
+                formatted = message;
+            if (arg0 != null)
+                formatted = string.Format(message + "{0}", arg0);
+            Console.WriteLine(formatted);
+        }
+    }
+
+}
---
+++ src/2_Builder/Program.cs
@@ -0,0 +1,16 @@
+﻿using _0_Util;
+
+namespace _2_Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            "Merhaba Dünya!".WriteToConsole();
+            "Builder Pattern Örneği\n".WriteToConsole();
+
+            "\nDevam etmek için enter e basınız".WriteToConsole();
+            System.Console.ReadLine();
+        }
+    }
+}
