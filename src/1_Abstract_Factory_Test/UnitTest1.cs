66de72e89724f9b54d92a81125ef7cfde927c7ed
Halil Koca <ihalilkoca@gmail.com>
2020-01-23 14:40:42 +0300

    Abstract Factory Pattern test edildi

--- src/1_Abstract_Factory_Test/UnitTest1.cs
+++ src/1_Abstract_Factory_Test/UnitTest1.cs
@@ -1,4 +1,6 @@
-using System;
+using _1_Abstract_Factory.AbstractFactory;
+using _1_Abstract_Factory.Factory;
+using _1_Abstract_Factory.Interface;
 using Xunit;
 
 namespace _1_Abstract_Factory_Test
@@ -6,9 +8,27 @@ namespace _1_Abstract_Factory_Test
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Macosx_Uretimi()
         {
+            ToolFactory factory = new MacosxFactory();
 
+            IDialog dialog = factory.CreateDialog();
+            dialog.GetDialog();
+
+            IToolbar toolbar = factory.CreateToolbar();
+            toolbar.GetToolbar();
+        }
+
+        [Fact]
+        public void Windows_Uretimi()
+        {
+            ToolFactory factory = new WindowsFactory();
+
+            IDialog dialog = factory.CreateDialog();
+            dialog.GetDialog();
+
+            IToolbar toolbar = factory.CreateToolbar();
+            toolbar.GetToolbar();
         }
     }
 }
